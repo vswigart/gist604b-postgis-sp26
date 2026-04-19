08a6459ead91da362b88666ba04ec74a501ae31f
Victoria Swigart <vswigart@arizona.edu>
2026-04-19 19:15:38 +0000

    Completed Exercise 02

--- sql/02_geometry_queries.sql
+++ sql/02_geometry_queries.sql
@@ -12,9 +12,12 @@
 -- Note: Units will be in square meters (CRS is UTM Zone 18N, EPSG:26918)
 
 -- TODO: Write your query below
-
-
-
+SELECT 
+    ST_Area(geom) AS new_brighton_area_sq_m
+FROM 
+    nyc_neighborhoods
+WHERE 
+    name = 'New Brighton';
 
 -- Exercise 2: What is the area of the Bronx in acres?
 -- Expected output: one row with the total area of the Bronx in acres
@@ -28,9 +31,12 @@
 -- Hint: Filter rows where boroname = 'The Bronx'
 
 -- TODO: Write your query below
-
-
-
+SELECT 
+    ST_Area(ST_Union(geom)) / 4046.86 AS bronx_area_acres
+FROM 
+    nyc_neighborhoods
+WHERE 
+    boroname = 'The Bronx';
 
 -- Exercise 3: How many census blocks in New York City DO NOT have a hole in them?
 -- Expected output: one row with the count of census blocks without holes
@@ -44,9 +50,12 @@
 -- Hint: ST_NumInteriorRings(geom) = 0 means no holes (interior rings)
 
 -- TODO: Write your query below
-
-
-
+SELECT 
+    COUNT(*) AS no_hole_polygons
+FROM 
+    nyc_census_blocks
+WHERE 
+    ST_NumInteriorRings(geom) = 0;
 
 -- Exercise 4: What is the total length of streets (in miles) in New York City?
 -- Expected output: one row with total street length in miles
@@ -60,9 +69,10 @@
 -- Hint: Use the nyc_streets table
 
 -- TODO: Write your query below
-
-
-
+SELECT 
+    SUM(ST_Length(geom)) / 1609.34 AS total_length_miles
+FROM 
+    nyc_streets;
 
 -- Exercise 5: How long is '5th Ave' in meters?
 -- Expected output: one row with the length of 5th Ave
@@ -76,9 +86,12 @@
 -- Hint: Filter rows where name = '5th Ave'
 
 -- TODO: Write your query below
-
-
-
+SELECT 
+    SUM(ST_Length(geom)) AS fifth_ave_length
+FROM 
+    nyc_streets
+WHERE 
+    name = '5th Ave';
 
 -- Exercise 6: What is the JSON representation of the boundary of 'Soho'?
 -- Expected output: one row with GeoJSON representation
@@ -91,9 +104,12 @@
 -- Hint: Filter rows where name = 'Soho'
 
 -- TODO: Write your query below
-
-
-
+SELECT 
+    ST_AsGeoJSON(geom) AS soho_geojson
+FROM 
+    nyc_neighborhoods
+WHERE 
+    name = 'Soho';
 
 -- Exercise 7: How many polygons are in the 'Coney Island' multipolygon?
 -- Expected output: one row with the number of polygons
@@ -105,8 +121,12 @@
 -- Hint: Use num_coney_island_polygons as the output alias
 -- Hint: Use the nyc_neighborhoods table
 -- Hint: Filter rows where name = 'Coney Island'
-
-
+SELECT 
+    ST_NumGeometries(geom) AS num_coney_island_polygons
+FROM 
+    nyc_neighborhoods
+WHERE 
+    name = 'Coney Island';
 
 -- Exercise 8: What are the 5 longest roads in NYC?
 -- Expected output: five rows with road names and lengths in meters
@@ -123,6 +143,13 @@
 -- Hint: Use the nyc_streets table
 -- Hint: ORDER BY ST_Length(geom) DESC OR use the alias length_meters
 -- Hint: Use LIMIT 5
-
-
-
+SELECT 
+    name,
+    SUM(ST_Length(geom)) AS length_meters
+FROM 
+    nyc_streets
+GROUP BY 
+    name
+ORDER BY 
+    length_meters DESC
+LIMIT 5;
